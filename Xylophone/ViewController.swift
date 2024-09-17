57e6b650e3bd400ccf19a6d584bfe22628684c80
Oksana ogay <example@email.com>
2024-09-17 11:57:08 +0500

    Create buttons

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -10,12 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var nameButtons = ["A", "C", "B", "F", "G", "E", "D"]
+    private var buttonStackView = UIStackView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        setViews()
+        setConstraints()
+        createButtons()
+    }
+    
+    private func createButtons() {
+        for (index, nameButton) in nameButtons.enumerated() {
+            let multiplierWidth = 0.97 - (0.03 * Double(index))
+            createButton(name: nameButton, width: multiplierWidth)
+        }
+    }
+    private func createButton(name: String, width: Double) {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(name, for: .normal)
+        button.tintColor = .white
+        button.titleLabel?.font = .systemFont(ofSize: 45)
+        button.addTarget(self, action: #selector(buttonsTupped), for: .touchUpInside)
+        
+        
+        buttonStackView.addArrangedSubview(button)
+        button.layer.cornerRadius = 10
+        button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: width).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        button.backgroundColor = .red
     }
 
-
+    @objc private func buttonsTupped(_ sender: UIButton) {
+        print(sender.currentTitle)
+    }
     
 
+}
+extension ViewController {
+    private func setViews() {
+        view.addSubview(buttonStackView)
+        buttonStackView.alignment = .center
+        buttonStackView.axis = .vertical
+        buttonStackView.spacing = 10
+        buttonStackView.distribution = .fillEqually
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+      
+        
+    }
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            buttonStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            buttonStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            buttonStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        
+        ])
+        
+    }
+}
+#Preview {
+    ViewController()
 }
 
